7afe64c089845fe239e06770b5f09d2502c56aaf
YukiTinNguyen <cubernguyen123@gmail.com>
2023-11-28 16:10:57 +0700

    Bai2 thoi gian

--- Bai2Trinh.py
+++ Bai2Trinh.py
@@ -1,14 +1,17 @@
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
 from sklearn.metrics import accuracy_score, confusion_matrix
 import seaborn as sns
 import pandas as pd
-from scipy.signal import windows
-import time
+from sklearn.decomposition import PCA
+from scipy.signal import hamming
+import scipy.signal.windows
+
 
 def segment_vowel_silence(audio, Fs, threshold = 0.04, min_duration=0.3):
-
+    # Chia khung tín hiệu, mỗi khung độ dài 25ms
     frame_length = int(0.02 * Fs)
     frames = librosa.util.frame(audio, frame_length=frame_length, hop_length=frame_length)
     # Tính STE từng khung
@@ -23,7 +26,6 @@ def segment_vowel_silence(audio, Fs, threshold = 0.04, min_duration=0.3):
     is_speech_full = np.repeat(is_speech, frame_length)[:len(audio)]
     is_speech_full = np.pad(is_speech_full, (0, len(audio) - len(is_speech_full)), constant_values=False) 
     
-
     # Tìm danh sách khoảng lặng
     silence_segments = librosa.effects.split(audio, top_db=threshold)
 
@@ -33,48 +35,54 @@ def segment_vowel_silence(audio, Fs, threshold = 0.04, min_duration=0.3):
         if duration < min_duration:
             is_speech_full[start:end] = True
 
-    # Tìm vị trí của các khung nguyên âm
-    vowel_indices = np.where(is_speech_full)[0]
-
-    # Chia thành 3 đoạn và lấy đoạn giữa
-    if len(vowel_indices) >= 3:
-        start_index = vowel_indices[len(vowel_indices) // 3]
-        end_index = vowel_indices[2 * len(vowel_indices) // 3]
-        vowel_middle_segment = audio[start_index:end_index]
-    else:
-        vowel_middle_segment = audio
-
-    return vowel_middle_segment
+    # Trả về tín hiệu chỉ chứa nguyên âm hay tiếng nói
+    vowel = audio[is_speech_full]
+    return vowel
 
 def nomalizing_value(fft_vector):
     magnitude_spectrum = np.abs(fft_vector)
     normalized_spectrum = magnitude_spectrum / np.sum(magnitude_spectrum)
     return normalized_spectrum
 
-def FFT_1vowel_1speaker(audio, Fs, N_FFT=512):
+def FFT_1vowel_1speaker(audio, Fs , N_FFT):
     """
     Hàm Trích xuất vector FFT của 1 nguyên âm 1 người (1 audio input)
     """
-
-    # Chia khung tín hiệu, mỗi khung độ dài 25ms
     frame_length = int(0.025 * Fs)
     hop_length = int(0.01 * Fs)
-    frames = librosa.util.frame(audio, frame_length=frame_length, hop_length=hop_length)
-
+    frames = librosa.util.frame(x=audio, frame_length=frame_length, hop_length=hop_length)
     sum_fft = np.zeros(N_FFT, dtype=complex)
-    for frame in (frames.T):  
-        #Vì thư viện Librosa lưu trữ mỗi khung theo từng cột, \
-        # do đó ta cần chuyển vị để các khung sắp xếp theo từng hàng thì for each mới đúng
-        windowing_frame = frame * windows.hamming(frame_length)
+
+    for frame in frames.T:  # .T Chuyển đổi khung để lặp lại chính xác
+        windowing_frame = frame * scipy.signal.windows.hamming(frame_length)
         sum_fft += np.abs(np.fft.fft(windowing_frame, N_FFT))
 
-    avg_fft = sum_fft / len(frames[0])  # Chia cho số lượng khung
+    avg_fft = sum_fft / len(frames[0])  # Chia cho số lượng khung hình
+    return nomalizing_value(avg_fft)[:N_FFT // 2]
 
-    return nomalizing_value(avg_fft)[:N_FFT // 2] #Vì vector FFT có tính đối xứng nên chỉ cần lấy 1 nửa
+# Chia khung tín hiệu, mỗi khung độ dài 20ms,số mẫu mỗi khung = độ dài khung *  tần số lấy mẫu
+    
+    #Số khung
+    # N = frames.shape[1]
+    # start = N // 3
+    # end = 2*start
+    # fft_frames = []
+    # # hamming dùng để giảm rò rỉ quang phổ khi thực hiện biến đổi Fourier trên 1 đoạn tín hiệu, cải thiện độ chính xác
+    # hanning_window = np.hanning(frame_length)
 
-def FFT_1vowel_nspeaker(vowel_label, N_FFT):
+    # for i in range(start, end):
+    #     frame = frames[:, i] * hanning_window # Áp dụng cửa sổ Hamming [:, i], chọn tất cả các hàng cột thứ i
+    #     fft_result = np.fft.fft(frame, N_FFT)
+    #     fft_frames.append(fft_result)
+
+    # # Tính trung bình cộng của M vector FFT
+    # avg_fft = np.mean(fft_frames, axis=0)
+
+    # return avg_fft
+
+def FFT_1vowel_nspeaker(vowelchar, N_FFT):  
     """ Hàm tính vector đặc trưng fft cho 1 nguyên âm (không phụ thuộc người nói)
-        - Đầu vào là 1 ký hiệu nguyên âm = nhãn ('a',.., 'u')  = tên tệp
+        - Đầu vào là 1 ký hiệu nguyên âm ('a',.., 'u') = tên tệp
         - Bằng cách tính trung bình cộng của 21 người nói khác nhau
         - Trả về 1 vector fft cuối cùng ---> để bỏ vào model
     """
@@ -84,31 +92,35 @@ def FFT_1vowel_nspeaker(vowel_label, N_FFT):
     vectors = []
     
     for foldername in name_folders:
-        file_path = file_path_template.format(foldername, vowel_label)
-        print(file_path) #Dòng này sau này xóa
+        file_path = file_path_template.format(foldername, vowelchar)
+        # print(file_path) #Dòng này sau này xóa
         audio, Fs = librosa.load(file_path, sr=None)
-        vowel = segment_vowel_silence(audio, Fs)
-        fft1 = FFT_1vowel_1speaker(vowel, Fs, N_FFT=N_FFT)
+        vowel = segment_vowel_silence(audio, Fs, threshold = 0.065, min_duration=0.3)
+        fft1 = FFT_1vowel_1speaker(vowel,Fs, N_FFT=N_FFT)
         vectors.append(fft1)
 
     vector_fft = np.mean(vectors, axis=0)
-    print(f"Đã xong chữ {vowel_label}, len(vector_fft) = {len(vector_fft)}")
+    print(f"Đã xong chữ {vowelchar}, len(vector_fft) = {len(vector_fft)}")
     return vector_fft
+# vector_fft là tổng trung bình của 21 người nói khác nhau của mỗi nguyên âm
+# Câu 2. ý 3: ở câu 2c ta tính được nguyên âm 1 file của 1 người nói, 
+# giờ ta tính 5 file nguyên âm của 1 người nói và so sánh với trung bình của nguyên âm tương ứng \
+# bằng hàm matching để tính khoảng cách Euclidean
 
 def matching(vector_x, model_vectors):
     """Hàm so khớp vector_x (input) và model (các vector tham số của 5 nguyên âm)
     * Đầu vào:
       - vector_x: vector đặc trưng của 1 file tín hiệu kiểm thử
       - model là Bộ Vector tham số fft của 5 nguyên âm
-        # model_vectors[0] = vector_a,
-        # model_vectors[1] = vector_e,
-        # model_vectors[2] = vector_i,
-        # model_vectors[3] = vector_o,
-        # model_vectors[4] = vector_u]
+         model_vectors[0] = vector_a,
+         model_vectors[1] = vector_e,
+         model_vectors[2] = vector_i,
+         model_vectors[3] = vector_o,
+         model_vectors[4] = vector_u]
     * Đầu ra:
         Trả về kết quả là Nguyên âm có khoảng cách Euclid nhỏ nhất
     """
-    # Các nhãn
+    # Các nhãn tương ứng
     vowels = ['a', 'e', 'i', 'o', 'u']
     # Tính khoảng cách Euclidean giữa vector_x và từng vector trong model
     distances = [np.linalg.norm(vector_x - model_vector) for model_vector in model_vectors]
@@ -132,15 +144,20 @@ def readSignals_and_extractionFFT(list_path, N_FFT):
     fft_vectors = []
     for file_path in list_path:
         audio, Fs = librosa.load(file_path, sr=None)
-        vowel = segment_vowel_silence(audio, Fs)
-        fft1 = FFT_1vowel_1speaker(vowel, Fs, N_FFT=N_FFT)
+        vowel = segment_vowel_silence(audio, Fs, threshold = 0.065, min_duration=0.3)
+        fft1 = FFT_1vowel_1speaker(vowel,Fs, N_FFT=N_FFT)
         fft_vectors.append(fft1)
     return fft_vectors
 
 def test(x_test, y_test, model, N_FFT): 
-    """ Hàm dự đoán 1 tập dữ liệu kiểm thử
-        x_test: tập kiểm thử với kiểu dữ liệu là .......
-        y_test: nhãn của tập kiểm thử
+    """ Hàm dự đoán 1 tập dữ liệu kiểm thử và tính độ chính xác
+        Gọi hàm readSignals_and_extractionFFT để tính vector đặc trưng cho tất cả các file trong tập kiểm thử.
+        Dùng hàm matching để dự đoán nhãn của từng file.
+        In kết quả dự đoán và tính độ chính xác sử dụng accuracy_score.
+        
+        Đầu vào:
+        - x_test: tập kiểm thử với kiểu dữ liệu là .......
+        - y_test: nhãn của tập kiểm thử
 
         Trả về:
         - Kết quả nhận dạng (dự đoán) nhãn nguyên âm của mỗi file test (/a/, …,/u/), Đúng/Sai
@@ -159,14 +176,14 @@ def test(x_test, y_test, model, N_FFT):
     return y_pred, accuracy
 
 def plot_vector(vector, label):
-    #Hàm vẽ 1 vector
+    #Hàm vẽ 1 vector (subplot)
     plt.plot(np.real(vector), label=label)
     plt.xlabel('Dimension')
     plt.ylabel('Real Value')
     plt.legend()
 
 def plot_all_vectors(vectors, labels):
-    #Hàm vẽ 5 vector/1 đồ thị
+    #Hàm vẽ 5 vector trong cùng 1 đồ thị
     for i, vector in enumerate(vectors):
         plt.plot(np.real(vector), label=labels[i])
     plt.xlabel('Dimension')
@@ -174,15 +191,15 @@ def plot_all_vectors(vectors, labels):
     plt.legend()
 
 if __name__ == "__main__":
-    #Đọc tên từng file, bỏ vào x_test và y_test
-    # train_folders = ["23MTL", "24FTL", "25MLM", "27MCM", "28MVN", "29MHN", "30FTN", "32MTP", "33MHP", "34MQP", "35MMQ",\
-    #      "36MAQ", "37MDS", "38MDS", "39MTS", "40MHS", "41MVS", "42FQT", "43MNT", "44MTT", "45MDV"]
+    
     test_folders = ['01MDA', '02FVA', '03MAB', '04MHB', '05MVB', '06FTB', '07FTC', '08MLD', '09MPD', '10MSD', '11MVD', \
         '12FTD', '14FHH', '15MMH', '16FTH', '17MTH', '18MNK', '19MXK', '20MVK', '21MTL', '22MHL']
     vowel_labels = ['a', 'e', 'i', 'o', 'u']
     file_path_template = 'signals/NguyenAmKiemThu-16k/{}/{}.wav'
     x_test = [] #Lưu đường dẫn từng file test
     y_test = [] #Lưu nhãn
+    
+    #Đọc tên từng file, bỏ vào x_test và y_test
     for folder in test_folders:
         for label in vowel_labels:
             file_path = file_path_template.format(folder, label)
@@ -206,21 +223,19 @@ if __name__ == "__main__":
     plot_all_vectors(model1, labels=vowel_labels)
     plt.show()
 
-    start1 = time.time()
     y_pred1, accuracy1 = test(x_test, y_test, model1, 512)
-    start2 = time.time()
     y_pred2, accuracy2 = test(x_test, y_test, model2, 1024)
-    start3 = time.time()
     y_pred3, accuracy3 = test(x_test, y_test, model3, 2048)
-    end3 = time.time()
-
-    print(f"Thời gian chạy với N_FFT = 512: {start2 - start1} s")
-    print(f"Thời gian chạy với N_FFT = 1024: {start3 - start2} s")
-    print(f"Thời gian chạy với N_FFT = 2048: {end3 - start3} s")
 
     print(accuracy1, accuracy2, accuracy3, sep='\n')
 
-    confusion = confusion = confusion_matrix(y_test, y_pred1)
+    confusion = None
+    if (accuracy1 > accuracy2 and accuracy1 > accuracy3):
+        confusion = confusion_matrix(y_test, y_pred1)
+    elif (accuracy2 > accuracy3):
+        confusion = confusion_matrix(y_test, y_pred2)
+    else:
+        confusion = confusion_matrix(y_test, y_pred3)
     
     class_names = np.unique(y_test)
     df_confusion = pd.DataFrame(confusion, index=class_names, columns=class_names)
---
+++ test-module.py
@@ -0,0 +1,238 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+import pandas as pd
+from sklearn.decomposition import PCA
+
+def segment_vowel_silence(audio, Fs, threshold = 0.065, min_duration=0.3):
+    print("check", Fs)
+    # Chia khung tín hiệu, mỗi khung độ dài 25ms
+    frame_length = int(0.025 * Fs)
+    frames = librosa.util.frame(audio, frame_length=frame_length, hop_length=frame_length)
+    # Tính STE từng khung
+    ste = np.sum(np.square(frames), axis=0)
+
+    # Chuẩn hóa STE
+    ste_normalized = (ste - np.min(ste)) / (np.max(ste) - np.min(ste))
+
+    # Phân loại thành tiếng nói và khoảng lặng
+    is_speech = ste_normalized > threshold
+
+    is_speech_full = np.repeat(is_speech, frame_length)[:len(audio)]
+    is_speech_full = np.pad(is_speech_full, (0, len(audio) - len(is_speech_full)), constant_values=False) 
+    
+
+    # Tìm danh sách khoảng lặng
+    silence_segments = librosa.effects.split(audio, top_db=threshold)
+
+    # Bỏ đi các khoảng lặng < 300 ms
+    for start, end in silence_segments:
+        duration = librosa.samples_to_time(end - start, sr=Fs)
+        if duration < min_duration:
+            is_speech_full[start:end] = True
+
+    # Tìm vị trí của các khung nguyên âm
+    vowel_indices = np.where(is_speech_full)[0]
+
+    # Chia thành 3 đoạn và lấy đoạn giữa
+    if len(vowel_indices) >= 3:
+        start_index = vowel_indices[len(vowel_indices) // 3]
+        end_index = vowel_indices[2 * len(vowel_indices) // 3]
+        vowel_middle_segment = audio[start_index:end_index]
+    else:
+        vowel_middle_segment = audio
+
+    return vowel_middle_segment
+
+#  trich xuat vector dac trung cho tung file tin hieu
+def nomalizing_value(mfcc_vector):
+    """
+    Hàm chuẩn hóa vector MFCC về cùng thang đo
+    """
+    return (mfcc_vector - np.mean(mfcc_vector)) / np.std(mfcc_vector)
+
+def extract_mfcc_from_wav(file_path):
+    # Đọc file wav và lấy mẫu
+    signal, Fs = librosa.load(file_path, sr=None)
+    vowel = segment_vowel_silence(signal, Fs, threshold=0.065, min_duration=0.3)
+    frame_length = int(0.025 * Fs)
+    frames = librosa.util.frame(vowel, frame_length=frame_length, hop_length=frame_length // 2)
+
+    mfcc_frames = []
+
+    for frame in frames.T:
+        mfcc_result = librosa.feature.mfcc(y=frame, sr=Fs, n_mfcc=13, n_fft=frame_length, hop_length=frame_length//2)
+        mfcc_frames.append(mfcc_result)
+
+    # Trích xuất MFCC
+    mfccs = np.mean(mfcc_frames, axis=2)  # Take mean along the time axis
+    # được sử dụng để lấy giá trị trung bình dọc theo trục thời gian (axis=2), 
+    # tính trung bình một cách hiệu quả các hệ số MFCC theo thời gian cho mỗi khung.
+    # Nói cách khác, nó tính giá trị trung bình của các hệ số MFCC theo thời gian cho từng khung, 
+    # tạo ra một mảng 2D trong đó mỗi hàng tương ứng với một hệ số MFCC và mỗi cột tương ứng với một khung. 
+    # Điều này cho phép bạn có được bộ hệ số MFCC đại diện cho từng khung hình trong âm thanh đầu vào.
+
+    # Chuẩn hóa MFCCs
+    normalized_mfccs = []
+    for mfcc_frame in mfccs:
+        normalized_mfcc_frame = nomalizing_value(mfcc_frame)
+        normalized_mfccs.append(normalized_mfcc_frame)
+
+    # Tính giá trị trung bình của các frame MFCC chuẩn hóa
+    mfccs_mean = np.mean(normalized_mfccs, axis=0)
+
+    return mfccs_mean
+
+def build_model_a():
+    name_folders = ["23MTL", "24FTL", "25MLM", "27MCM", "28MVN", "29MHN", "30FTN", "32MTP", "33MHP", "34MQP", "35MMQ",
+                    "36MAQ", "37MDS", "38MDS", "39MTS", "40MHS", "41MVS", "42FQT", "43MNT", "44MTT", "45MDV"]
+    file_path_template = 'signals/NguyenAmHuanLuyen-16k/{}/a.wav'
+    vectors_a = []
+    for foldername in name_folders:
+        file_path = file_path_template.format(foldername)
+        MFCC = extract_mfcc_from_wav(file_path)
+        vectors_a.append(MFCC)
+    avg_vector_a = np.mean(vectors_a,axis=0)
+    return avg_vector_a
+
+def build_model_e():
+    name_folders = ["23MTL", "24FTL", "25MLM", "27MCM", "28MVN", "29MHN", "30FTN", "32MTP", "33MHP", "34MQP", "35MMQ",
+                    "36MAQ", "37MDS", "38MDS", "39MTS", "40MHS", "41MVS", "42FQT", "43MNT", "44MTT", "45MDV"]
+    file_path_template = 'signals/NguyenAmHuanLuyen-16k/{}/e.wav'
+    vectors_e = []
+    for foldername in name_folders:
+        file_path = file_path_template.format(foldername)
+        MFCC = extract_mfcc_from_wav(file_path)
+        vectors_e.append(MFCC)
+    avg_vector_e = np.mean(vectors_e,axis=0)
+    return avg_vector_e
+
+def build_model_i():
+    name_folders = ["23MTL", "24FTL", "25MLM", "27MCM", "28MVN", "29MHN", "30FTN", "32MTP", "33MHP", "34MQP", "35MMQ",
+                    "36MAQ", "37MDS", "38MDS", "39MTS", "40MHS", "41MVS", "42FQT", "43MNT", "44MTT", "45MDV"]
+    file_path_template = 'signals/NguyenAmHuanLuyen-16k/{}/i.wav'
+    vectors_i = []
+    for foldername in name_folders:
+        file_path = file_path_template.format(foldername)
+        MFCC = extract_mfcc_from_wav(file_path)
+        vectors_i.append(MFCC)
+    avg_vector_i = np.mean(vectors_i,axis=0)
+    return avg_vector_i
+
+def build_model_o():
+    name_folders = ["23MTL", "24FTL", "25MLM", "27MCM", "28MVN", "29MHN", "30FTN", "32MTP", "33MHP", "34MQP", "35MMQ",
+                    "36MAQ", "37MDS", "38MDS", "39MTS", "40MHS", "41MVS", "42FQT", "43MNT", "44MTT", "45MDV"]
+    file_path_template = 'signals/NguyenAmHuanLuyen-16k/{}/o.wav'
+    vectors_o = []
+    for foldername in name_folders:
+        file_path = file_path_template.format(foldername)
+        MFCC = extract_mfcc_from_wav(file_path)
+        vectors_o.append(MFCC)
+    avg_vector_o = np.mean(vectors_o,axis=0)
+    return avg_vector_o
+
+def build_model_u():
+    name_folders = ["23MTL", "24FTL", "25MLM", "27MCM", "28MVN", "29MHN", "30FTN", "32MTP", "33MHP", "34MQP", "35MMQ",
+                    "36MAQ", "37MDS", "38MDS", "39MTS", "40MHS", "41MVS", "42FQT", "43MNT", "44MTT", "45MDV"]
+    file_path_template = 'signals/NguyenAmHuanLuyen-16k/{}/u.wav'
+    vectors_u = []
+    for foldername in name_folders:
+        file_path = file_path_template.format(foldername)
+        MFCC = extract_mfcc_from_wav(file_path)
+        vectors_u.append(MFCC)
+    avg_vector_u = np.mean(vectors_u,axis=0)
+    return avg_vector_u
+
+def Model_Of_speak():
+    model_a = build_model_a()
+    model_e = build_model_e()
+    model_i = build_model_i()
+    model_o = build_model_o()
+    model_u = build_model_u()
+    model = np.array([model_a, model_e, model_i, model_o, model_u])
+    plt.figure(figsize=(10, 6))
+    for i in range(5):
+        plt.plot(model[i, :], label=f'MFCC {i + 1}')
+    # Thêm title, legend và labels
+    plt.title(' 5 MFCC Vectors Over Time')
+    plt.xlabel('Frame Index')
+    plt.ylabel('MFCC Coefficient Value')
+    plt.legend()
+    plt.show()
+    return model
+
+def readSignals_and_extractionMFCC(list_path):
+    mfcc_vectors = []
+    for file_path in list_path:
+        MFCC = extract_mfcc_from_wav(file_path)
+        mfcc_vectors.append(MFCC)
+    return mfcc_vectors
+
+def test(x_test, y_test, model):
+    """ Hàm dự đoán 1 tập dữ liệu kiểm thử
+        x_test: file kiểm thử với kiểu dữ liệu là .......
+        y_test: nhãn của tập kiểm thử
+
+        Trả về:
+        - Kết quả nhận dạng (dự đoán) nhãn nguyên âm của mỗi file test (/a/, …,/u/), Đúng/Sai
+        - Độ chính xác nhận dạng tổng hợp (%)
+    """
+    y_pred = []
+    test_fft_vectors = readSignals_and_extractionMFCC(x_test)
+    for i in range(len(test_fft_vectors)):
+        one_predict = matching(test_fft_vectors[i], model)
+        y_pred.append(one_predict)
+        check = (y_test[i] == one_predict)
+        print(f"{x_test[i]} /{one_predict}/ -> {check}")
+
+    accuracy = accuracy_score(y_test, y_pred)
+    return y_pred, accuracy
+
+def matching(vector_x, model_vectors):
+    """Hàm so khớp vector_x (input) và model (các vector tham số của 5 nguyên âm)
+        Trả về kết quả là Nguyên âm có khoảng cách Euclid nhỏ nhất
+    """
+    # Danh sách các vector tham số fft của 5 nguyên âm
+    # model_vectors[0] = vector_a,
+    # model_vectors[1] = vector_e,
+    # model_vectors[2] = vector_i,
+    # model_vectors[3] = vector_o,
+    # model_vectors[4] = vector_u]
+    # Các nhãn tương ứng
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    # Tính khoảng cách Euclidean giữa vector_x và từng vector trong model
+    distances = [np.linalg.norm(vector_x - model_vector) for model_vector in model_vectors]
+    # Xác định nguyên âm có khoảng cách nhỏ nhất
+    min_distance_index = np.argmin(distances)
+    # Kết quả nhận dạng
+    result = vowels[min_distance_index]
+    return result
+
+
+test_folders = ['01MDA', '02FVA', '03MAB', '04MHB', '05MVB', '06FTB', '07FTC', '08MLD', '09MPD', '10MSD', '11MVD','12FTD', '14FHH', '15MMH', '16FTH', '17MTH', '18MNK', '19MXK', '20MVK', '21MTL', '22MHL']
+vowel_labels = ['a', 'e', 'i', 'o', 'u']
+file_path_template = 'signals/NguyenAmKiemThu-16k/{}/{}.wav'
+x_test = []  # Lưu đường dẫn từng file test
+y_test = []  # Lưu nhãn
+for folder in test_folders:
+    for label in vowel_labels:
+        file_path = file_path_template.format(folder,label)
+        x_test.append(file_path)
+        y_test.append(label)
+for x_name in x_test:
+    print(x_name)
+
+model = Model_Of_speak()
+y_pred, accuracy = test(x_test, y_test, model)
+confusion = confusion_matrix(y_test, y_pred)
+class_names = np.unique(y_test)
+df_confusion = pd.DataFrame(confusion, index=class_names, columns=class_names)
+plt.figure(figsize=(8, 6))
+sns.heatmap(df_confusion, annot=True, fmt="d", cmap="viridis")
+plt.xlabel("Predicted")
+plt.ylabel("True")
+plt.title("Confusion Matrix")
+plt.show()
+print(accuracy)
